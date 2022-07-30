d3513795176a21b4efe583d2fba5a50be6423d8d
ruqiya khalif <ruqiya101@gmail.com>
2022-07-30 17:39:52 +0100

    dino booking mvc complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_144644) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_161045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "dinosaur_id", null: false
+    t.integer "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dinosaur_id"], name: "index_bookings_on_dinosaur_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "dinosaurs", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "age"
+    t.text "diet"
+    t.string "origin"
+    t.string "image_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dinosaurs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_30_144644) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "dinosaurs"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "dinosaurs", "users"
 end
