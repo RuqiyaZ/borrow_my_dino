3121a77ddc057491b3fc2ed580ebba766bd84ae0
yulin zhou <zhouyul.uk@gmail.com>
2022-08-02 21:19:52 +0100

    booking draft done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+
+  def booking
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(params[:booking_id])
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:id, :date, :user_id)
+    end
 end
