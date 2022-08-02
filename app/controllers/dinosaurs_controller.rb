22cb4c15781ad8a5d48a59f5ed5effec65dddb2d
ruqiya khalif <ruqiya101@gmail.com>
2022-08-02 20:13:28 +0100

    private params method added to dino controller

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,2 +1,10 @@
 class DinosaursController < ApplicationController
+  private
+  def set_dinosaur
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :description, :age, :diet, :origin, :image_url)
+  end
 end
