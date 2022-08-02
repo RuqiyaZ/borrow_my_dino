b198b8e45d3da0a02b31e9f327ab5edbcc36430d
ruqiya khalif <ruqiya101@gmail.com>
2022-08-02 20:33:40 +0100

    validation for dino model and image url dropped

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -3,14 +3,14 @@ class DinosaursController < ApplicationController
   def show
     @dinosaur = Dinosaur.find(params[:id])
   end
-  
+
   private
-  
+
   def set_dinosaur
     @dinosaur = Dinosaur.find(params[:id])
   end
 
   def dinosaur_params
-    params.require(:dinosaur).permit(:name, :description, :age, :diet, :origin, :image_url)
+    params.require(:dinosaur).permit(:name, :description, :age, :diet, :origin)
   end
 end
