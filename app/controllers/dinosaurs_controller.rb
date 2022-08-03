a00304f10bafe701f57aa8acd6cb33804d18d244
ruqiya khalif <ruqiya101@gmail.com>
2022-08-03 01:00:29 +0100

    linked dino view to bookings view and updated the controllers

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -1,26 +1,30 @@
 class DinosaursController < ApplicationController
-  before_action :set_dinosaur, only: [ 'show', 'edit', 'update', 'destroy' ]
+  before_action :set_dinosaur, only: ['show']
 
   def new
     @dinosaurs = Dinosaur.new
   end
 
+  def index
+    @dinosaur = Dinosaur.all
+  end
+
   def show
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-    @dinosaur.update(dinosaur_params)
-    flash[notice:] = "Booking was successfully updated."
-    redirect_to dinosaurs_path(@dinosaur)
-  end
+  # def update
+  #   @dinosaur.update(dinosaur_params)
+  #   flash[notice:] = "Booking was successfully updated."
+  #   redirect_to dinosaurs_path(@dinosaur)
+  # end
 
-  def destroy
-    @dinosaur.destroy
-    redirect_to dinosaurs_path(@dinosaur)
-  end
+  # def destroy
+  #   @dinosaur.destroy
+  #   redirect_to dinosaurs_path(@dinosaur)
+  # end
 
   private
 
