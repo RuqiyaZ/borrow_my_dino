0c2d27b0310eaf4369b697681ce9fc29e8c96932
yulin zhou <zhouyul.uk@gmail.com>
2022-08-11 18:40:03 +0100

    fixes and confirm page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :dinosaurs do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :show]
   end
   resources :bookings, only: [:show, :destroy]
 end
