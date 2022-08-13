6a1d5b84a570ea5fc7e7e4d1d13ff33b9df04c54
ruqiya khalif <ruqiya101@gmail.com>
2022-08-13 11:55:18 +0100

    delete button red and params and model updated

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,6 +1,6 @@
 class Dinosaur < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :description, :age, :diet, :origin, presence: true
   validates :name, uniqueness: true
   has_one_attached :photo
